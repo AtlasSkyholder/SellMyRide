8ea2945c0edc660e7846237f6cdd1f3aea621c47
morbi <the_morbidus@hotmail.com>
2020-04-25 19:20:32 -0400

    update to ports and database paths

--- ride-api/src/db/index.js
+++ ride-api/src/db/index.js
@@ -1,7 +1,19 @@
 const pg = require("pg");
 
-const client = new pg.Client({
+// for local postgresql database
+
+/* const client = new pg.Client({
   connectionString: process.env.DATABASE_URL || ""
+}); */
+
+// for outside postgresql database like in AWS RDS
+
+const client = new pg.Client({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT
 });
 
 client
